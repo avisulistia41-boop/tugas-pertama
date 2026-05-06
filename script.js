ff9504faffc3a28dc4778ed7124d045df4c643d3
avisulistia41-boop <avisulistia41@gmail.com>
2026-05-06 22:29:33 +0700

    Perubahan tugas 5

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const produk = [
   { nama: "Nasi Goreng", harga: 15000, kategori: "makanan", gambar: "https://dcostseafood.id/wp-content/uploads/2023/04/Nasi-Goreng-Spesial.jpg", deskripsi: "Nasi goreng spesial" },
   { nama: "Mie Ayam", harga: 12000, kategori: "makanan", gambar: "https://cdn.yummy.co.id/content-images/images/20210627/CMvqtCKUxrMBbPCQ09CtypnobrGJ5b50-31363234373635373931d41d8cd98f00b204e9800998ecf8427e.jpg?x-oss-process=image/resize,w_388,h_388,m_fixed,x-oss-process=image/format,webp", deskripsi: "Mie ayam enak" },
-  { nama: "Es Teh", harga: 5000, kategori: "minuman", gambar: "https://i.pinimg.com/736x/64/f9/04/64f904fa176bc6699ed488c40d02b414.jpg", deskripsi: "Minuman segar" },
-  { nama: "Jus Jeruk", harga: 8000, kategori: "minuman", gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4fhT3e9GN8kkHH4uS1UP-x4hIYjYlW6risA&s", deskripsi: "Sehat dan segar" },
-  { nama: "Keripik", harga: 10000, kategori: "snack", gambar: "https://img.lazcdn.com/g/p/1fe25f5d76235f8841eeaf172663d41b.jpg_960x960q80.jpg_.webp", deskripsi: "Snack renyah" }
+  { nama: "Es Milk Tea", harga: 5000, kategori: "minuman", gambar: "https://i.pinimg.com/736x/64/f9/04/64f904fa176bc6699ed488c40d02b414.jpg", deskripsi: "Minuman segar" },
+  { nama: "Salad Buah", harga: 8000, kategori: "minuman", gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4fhT3e9GN8kkHH4uS1UP-x4hIYjYlW6risA&s", deskripsi: "Sehat dan segar" },
+  { nama: "Basreng", harga: 10000, kategori: "snack", gambar: "https://img.lazcdn.com/g/p/1fe25f5d76235f8841eeaf172663d41b.jpg_960x960q80.jpg_.webp", deskripsi: "Snack renyah" }
 ];
 
 // KERANJANG
